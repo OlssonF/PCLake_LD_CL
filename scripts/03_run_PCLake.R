e00d86f83af00aefc56b4798cd69ed7681717e56
Freya Olsson <freya.olsson@ru.nl>
2026-08-04 09:38:19 +0200

    add QIn forcing and run clear and turbid together

--- scripts/03_run_PCLake.R
+++ scripts/03_run_PCLake.R
@@ -42,7 +42,7 @@ dirSave <- dirShell
 source(file.path(dirShell, "scripts", "R_system", "functions.R"))
 source(file.path(dirShell, "scripts", "R_system", "functions_PCLake.R")) 
 # use the functions from the pathway optimisation workcase for now
-source(file.path(dirShell, "work_cases", "PCLake_pathway_optimisation", "scripts/optim_functions.R"))
+source(file.path(dirShell, "work_cases", "PCLake_pathway_optimisation", "R/optim_functions.R"))
 ## 2. Loading DATM file ------------------
 lDATM_SETTINGS <- PCModelReadDATMFile_PCLakePlus(fileXLS  = fileDATM,folderTXT = 'input',
                                                  locDATM = "excel",
@@ -88,11 +88,17 @@ if (subset == T) {
 }
 
 # report the validation states
-val_states <- c('oChlaEpi', 'oO2WEpi', 'oPTotWEpi','oPTotWHyp', 'oNTotWEpi',  'oNTotWHyp', 'aSecchiT')
-lDATM_SETTINGS$auxils$iReport[which(rownames(lDATM_SETTINGS$auxils) %in% val_states)] <- 1 
 # plus some other states to be plotted
-diag_states <- c('uTmEpi', 'uTmHyp', 'uDepthMixMeas', 'uPLoadEpi', 'uNLoadEpi', 'oPO4WHyp',  'oPO4WEpi', 'aStrat')
-lDATM_SETTINGS$auxils$iReport[which(rownames(lDATM_SETTINGS$auxils) %in% diag_states)] <- 1 # report these in the output
+rep_vars <- c(
+  'oChlaEpi', 'oO2WEpi', 'oPTotWEpi','oPTotWHyp', 'oNTotWEpi',  'oNTotWHyp', 'aSecchiT',
+  'uTmEpi', 'uTmHyp', 'uDepthMixMeas', 'uDepthWEpi', 'uDepthWHyp', 'aStrat', 
+  'oPO4WHyp',  'oPO4WEpi', 'aDVeg', 'oO2WHyp',  'tPDifPO4Hyp', 'aDFiAd', 'aDFiJv', 'aDPisc', 
+  'aDError','aNError','aPError','aO2Error', 'aDepthError',# model errors
+  'uVWind', 'uLOut','uPLoadEpi', 'uNLoadEpi' # forcings
+)
+lDATM_SETTINGS$auxils$iReport[which(rownames(lDATM_SETTINGS$auxils) %in% rep_vars)] <- 1 # report these in the output
+
+
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## 5.  Make and adjust cpp files ----------------------------------------
@@ -101,7 +107,7 @@ lDATM_SETTINGS$auxils$iReport[which(rownames(lDATM_SETTINGS$auxils) %in% diag_st
 PCModelAdjustCPPfiles(dirSHELL = dirShell,
                       nameWORKCASE = nameWorkCase,
                       lDATM = lDATM_SETTINGS,
-                      nRUN_SET = 3) # using set2
+                      nRUN_SET = 2) # using set2
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## 6.  Compile the model ------------------------------------------------
@@ -112,10 +118,11 @@ PCModelCompileModelWorkCase(dirSHELL = dirShell,
 
 
 years_run <- lDATM_SETTINGS$run_settings['dReady', 'Set3']
+change_sets <-c('sSet2', 'sSet3')
 
 setwd(project_location)
 
-#i=1
+# i=1
 for (i in 1:length(lake_names_lookup)) {
   
   # Select one lake at a time
@@ -138,12 +145,18 @@ for (i in 1:length(lake_names_lookup)) {
   longitude_use <- lakes_portal_use |> 
     select(WBLONG) |> pull()
   
+  Qin_use <- lakes_portal_use |> 
+    mutate(RET_TIMEdays = RET_TIMEyrs * 365) |> # this value is in years, convert to days
+    mutate(QIn = (MNDP*1000)/RET_TIMEdays) |> # convert MNDP to mm
+    select(QIn) |> 
+    pull() 
+  
   # Set WB specific parameters -------------------------#
-  change_sets <- which(colnames(lDATM_SETTINGS$params) %in% c('sSet3', 'sSet3'))
   #-----------------------------------------------------#
-  lDATM_SETTINGS$params[which(rownames(lDATM_SETTINGS$params) == 'cFetch'), change_sets] <- sqrt(lakes_portal_use$WBSAREA*10000) # convert from Ha->m2 (typical fetch)
-  lDATM_SETTINGS$params[str_detect(rownames(lDATM_SETTINGS$params), 'cLAT'), change_sets] <- latitude_use # latitude
-  lDATM_SETTINGS$params[str_detect(rownames(lDATM_SETTINGS$params), 'cDepthWInit0'), change_sets] <- lakes_portal_use$MNDP # mean depth
+  lDATM_SETTINGS$params['cFetch', change_sets] <- sqrt(lakes_portal_use$WBSAREA*10000) # convert from Ha->m2 (typical fetch)
+  lDATM_SETTINGS$params['cLAT', change_sets] <- latitude_use # latitude
+  lDATM_SETTINGS$params['cDepthWInit0', change_sets] <- lakes_portal_use$MNDP # mean depth
+  lDATM_SETTINGS$params['cQInEpi', change_sets] <- Qin_use # Inflow
   
   # -----------------------------------------------------#
   ## SAGIS (nutrient loading) ---------------------
@@ -169,6 +182,9 @@ for (i in 1:length(lake_names_lookup)) {
   NLoad  <- loads_pclake |> 
     filter(variable == 'nitrate')
   
+  lDATM_SETTINGS$forcings$sSet2$mPLoadEpi$value <- PLoad$value[c(1:nrow(PLoad), nrow(PLoad))] 
+  lDATM_SETTINGS$forcings$sSet2$mNLoadEpi$value <- NLoad$value[c(1:nrow(NLoad), nrow(NLoad))] 
+  
   lDATM_SETTINGS$forcings$sSet3$mPLoadEpi$value <- PLoad$value[c(1:nrow(PLoad), nrow(PLoad))] 
   lDATM_SETTINGS$forcings$sSet3$mNLoadEpi$value <- NLoad$value[c(1:nrow(NLoad), nrow(NLoad))] 
   # FYI; repeat the last row, for some reason that I don't know the timeseries is longer in PCLake
@@ -182,7 +198,7 @@ for (i in 1:length(lake_names_lookup)) {
   flake_mixdepth <- read_csv(file.path(project_location, 'data', 'flake', lake_id, paste0(lake_id, '_MLpredictions.csv')),
                              show_col_types = F)
   flake_strat <- read_csv(file.path(project_location, 'data', 'flake', lake_id, paste0(lake_id, '_stratpredictions.csv')),
-                             show_col_types = F)
+                          show_col_types = F)
   
   # Repeat temperatures
   flake_temps <- flake_temps |> 
@@ -194,6 +210,11 @@ for (i in 1:length(lake_names_lookup)) {
             year = 1:years_run) |> 
     arrange(year, doy) |> 
     mutate(time = row_number())
+  
+  lDATM_SETTINGS$forcings$sSet2$mTempEpi$value <- flake_temps$Ts[c(1:nrow(flake_temps), nrow(flake_temps))] 
+  lDATM_SETTINGS$forcings$sSet2$mTempHyp$value <- flake_temps$Tb[c(1:nrow(flake_temps), nrow(flake_temps))] 
+  lDATM_SETTINGS$forcings$sSet2$mMixDepth$value <- flake_temps$h_ML[c(1:nrow(flake_temps), nrow(flake_temps))] 
+  lDATM_SETTINGS$forcings$sSet2$mStrat$value <- flake_temps$strat[c(1:nrow(flake_temps), nrow(flake_temps))] 
   
   lDATM_SETTINGS$forcings$sSet3$mTempEpi$value <- flake_temps$Ts[c(1:nrow(flake_temps), nrow(flake_temps))] 
   lDATM_SETTINGS$forcings$sSet3$mTempHyp$value <- flake_temps$Tb[c(1:nrow(flake_temps), nrow(flake_temps))] 
@@ -226,7 +247,10 @@ for (i in 1:length(lake_names_lookup)) {
     rename(!!!met_vars) |> 
     mutate(time = row_number(), 
            ws = sqrt(u10^2 + v10^2)) 
-   
+  
+  
+  lDATM_SETTINGS$forcings$sSet2$mVWind$value <- era5_met$ws[c(1:nrow(era5_met), nrow(era5_met))][c(1:((years_run*365)+1))] 
+  lDATM_SETTINGS$forcings$sSet2$mLOut$value <- era5_met$sr[c(1:nrow(era5_met), nrow(era5_met))] [c(1:((years_run*365)+1))]
   
   lDATM_SETTINGS$forcings$sSet3$mVWind$value <- era5_met$ws[c(1:nrow(era5_met), nrow(era5_met))][c(1:((years_run*365)+1))] 
   lDATM_SETTINGS$forcings$sSet3$mLOut$value <- era5_met$sr[c(1:nrow(era5_met), nrow(era5_met))] [c(1:((years_run*365)+1))]
@@ -243,17 +267,29 @@ for (i in 1:length(lake_names_lookup)) {
                                        dirSHELL = dirShell,
                                        nameWORKCASE = nameWorkCase)
   
+  message('Running turbid initials')
+  run_turbid <- PCmodelSingleRun(lDATM = lDATM_SETTINGS,
+                                 nRUN_SET = 2,
+                                 dfSTATES = InitStates,
+                                 integrator_method = "rk45ck", #euler
+                                 dirHOME = dirHome,
+                                 nameWORKCASE = nameWorkCase) 
+  message('Running clear initials')
+  run_clear <- PCmodelSingleRun(lDATM = lDATM_SETTINGS,
+                                nRUN_SET = 3,
+                                dfSTATES = InitStates,
+                                integrator_method = "rk45ck", #euler
+                                dirHOME = dirHome,
+                                nameWORKCASE = nameWorkCase) 
   
-  run1 <- PCmodelSingleRun(lDATM = lDATM_SETTINGS,
-                           nRUN_SET = 3,
-                           dfSTATES = InitStates,
-                           integrator_method = "rk45ck", #euler
-                           dirHOME = dirHome,
-                           nameWORKCASE = nameWorkCase) 
-  
-  
+
   dir.create(file.path(project_location, 'output'), showWarnings = F)
-  run1 |> 
+  run_turbid |> 
+    write_csv(file = file.path(project_location, 'output', 
+                               paste0('baseline_', lake_id, '-turbid.csv')),
+              progress = F)
+  
+  run_clear |> 
     write_csv(file = file.path(project_location, 'output', 
                                paste0('baseline_', lake_id, '-clear.csv')),
               progress = F)
