0fe2116c68ebaa5dbba78c1f5f35e0211a276550
Freya Olsson <freya.olsson@ru.nl>
2026-09-17 17:17:21 +0200

    use new validation data

--- scripts/b_model_validation.R
+++ scripts/b_model_validation.R
@@ -47,7 +47,6 @@ if (subset == T) {
 }
 
 
-
 ## Read in the model output -----------------#
 baseline_runs <- pclake_results |>  
   lapply(read_csv, show_col_types = F, id = 'filename') |> 
@@ -63,102 +62,67 @@ baseline_runs <- pclake_results |>
             by = join_by(WBID))
 
 # read in observations -----------------------
-
-## UKCEH -------------------------------------
-obs_secchi <- read_csv(file.path(val_dir,'UKCEH', 'Secchi.csv'), show_col_types = F) |> 
-  rename_with(tolower) |> 
-  mutate(date = ymd(date)) |> 
-  mutate(site = ifelse(site %in% c('SBAS', 'NBAS'), 'WIND', site)) |> 
-  left_join(distinct(data.frame(WBID = lake_names_lookup,
-                                site = names(lake_names_lookup))),
-            by = join_by(site)) |> 
-  filter(between(date, as_date('2000-01-01'), as_date('2024-12-31')))
-
-obs_tempDO <- read_csv(file.path(val_dir,'UKCEH',  'temperature and oxygen.csv'), show_col_types = F) |> 
-  rename_with(tolower) |> 
-  mutate(date = dmy(date)) |> 
-  mutate(site = ifelse(site %in% c('SBAS', 'NBAS'), 'WIND', site)) |> 
-  left_join(distinct(data.frame(WBID = lake_names_lookup,
-                                site = names(lake_names_lookup))),
-            by = join_by(site)) |> 
-  filter(between(date, as_date('2000-01-01'), as_date('2024-12-31')))
-
-# also requires the sample.csv that determines where the sample was taken
-# regular samples are integrated top 5/7 m at deepest location (7 in Windermere only), or DIP = shoreline
-
-samples_chem <- read_csv(file.path(val_dir,'UKCEH',  'samples.csv'), show_col_types = F) |> 
-  mutate(site = ifelse(site %in% c('SBAS', 'NBAS'), 'WIND', site))
-
-chem_vars <- c('TOCA') 
-
-obs_chem <- read_csv(file.path(val_dir,'UKCEH',  'chemistry.csv'), show_col_types = F) |>
-  mutate(date = ymd(date)) |> 
-  mutate(site = ifelse(site %in% c('SBAS', 'NBAS'), 'WIND', site)) |> 
-  filter(between(date, as_date('2000-01-01'), as_date('2024-12-31'))) |> 
-  left_join(distinct(data.frame(WBID = lake_names_lookup,
-                                site = names(lake_names_lookup))),
-            by = join_by(site)) |> 
-  left_join(samples_chem,
-            by = join_by(date, site), 
-            relationship = 'many-to-many') |> 
-  distinct() 
-
-
-## EA ------------------------------------------
-EA_sites <- readxl::read_xlsx('data/SITE ID_MULTIPLE DATA SOURCES_LD LAKES.xlsx') |> 
-  filter(!is.na(`sample.samplingPoint.notation_LD_Filtered2004 to LD_Filtered2024`)) |> 
-  mutate(WBID = zoo::na.locf(`WBID_Lake District_UKCEH Portal data_raw.xlsx`),
-         NAME = zoo::na.locf(`NAME_Lake District_UKCEH Portal data_raw.xlsx`))
-
-EA_obs <- lapply(list.files(file.path(val_dir, 'EA_WQ'), '.csv', full.names = T),
-                 read_csv, show_col_types = F) |> 
-  bind_rows()
+# make sure you have run 00_compile_obs.R
+obs <- read_csv('data/Validation/output/LD_combined_database_core_variables.csv') |> 
+  filter(date > ymd('1990-01-01')) |> 
+  mutate(site_id = ifelse(site_id %in% WIND, WIND_WBID, site_id))
 
 # plots
 
 (baseline_runs |> 
     select(Date, aSecchiT, NAME, WBID) |> 
-    left_join(obs_secchi, by = join_by(NAME == site, Date == date, WBID == WBID)) |> 
+    left_join(filter(obs, variable_code == 'SECC'),
+              by = join_by(Date == date, WBID == site_id)) |> 
     mutate(doy = yday(Date),
            year = year(Date)) |> 
-    ggplot(aes(x=doy, y = diskvalu)) +
+    ggplot(aes(x=doy, y = value)) +
     geom_point(size = 0.9, alpha = 0.3) +
     geom_line(aes(y = aSecchiT, group = year), alpha = 0.6, colour = 'goldenrod') + # PClake in mg/m3
     theme_bw() +
-    facet_wrap(~WBID+NAME, scales = 'free_y')) |> 
-  ggsave(filename = file.path(out_dir, 'plot', 'secchi_val_turbid.png'),
-         height = 20, width = 20, units = 'cm')
+    facet_wrap(~WBID+NAME, scales = 'free_y')) #|> 
+  # ggsave(filename = file.path(out_dir, 'plot', 'secchi_val_turbid.png'),
+  #        height = 20, width = 20, units = 'cm')
 
 (baseline_runs |> 
     select(Date, oChlaEpi, NAME, WBID) |> 
-    left_join(filter(obs_chem, variable == 'TOCA'), 
-              by = join_by(NAME == site, Date == date, WBID ==WBID)) |> 
+    left_join(filter(obs, variable_code == 'TOCA'), 
+              by = join_by(Date == date, WBID == site_id)) |> 
     mutate(doy = yday(Date),
            year = year(Date)) |> 
-    ggplot(aes(x=doy, y = chemvalu)) + # obs are in ug/L
+    ggplot(aes(x=doy, y = value)) + # obs are in ug/L
     geom_point(size = 0.9, alpha = 0.3) +
     geom_line(aes(y = oChlaEpi, group = year), alpha = 0.6, colour = 'seagreen') + # PClake in mg/m3
     theme_bw() +
-    facet_wrap(~WBID+NAME, scales = 'free_y')) |> 
-  ggsave(filename = file.path(out_dir, 'plot', 'chla_val_turbid.png'),
-         height = 20, width = 20, units = 'cm')
+    facet_wrap(~WBID+NAME, scales = 'free_y')) #|> 
+  # ggsave(filename = file.path(out_dir, 'plot', 'chla_val_turbid.png'),
+  #        height = 20, width = 20, units = 'cm')
 
 
 (baseline_runs |> 
     select(Date, oPTotWEpi, NAME, WBID) |> 
-    left_join(filter(obs_chem, variable == 'TOTP'), 
-              by = join_by(NAME == site, Date == date, WBID ==WBID)) |> 
+    left_join(filter(obs, variable_code == 'TOTP'), 
+              by = join_by(Date == date, WBID == site_id)) |> 
     mutate(doy = yday(Date),
            year = year(Date)) |> 
     
-    ggplot(aes(x=doy, y = chemvalu/1000)) + # obs are in ug/L
+    ggplot(aes(x=doy, y = value/1000)) + # obs are in ug/L
     geom_point(size = 0.9, alpha = 0.3) +
     geom_line(aes(y = oPTotWEpi, group = year), alpha = 0.6, colour = 'orchid4') + # PClake in g/m3
     theme_bw() +
-    facet_wrap(~WBID+NAME, scales = 'free_y')) |> 
-  ggsave(filename = file.path(out_dir, 'plot', 'TP_val_turbid.png'),
-         height = 20, width = 20, units = 'cm')
+    facet_wrap(~WBID+NAME, scales = 'free_y')) #|> 
+  # ggsave(filename = file.path(out_dir, 'plot', 'TP_val_turbid.png'),
+  #        height = 20, width = 20, units = 'cm')
 
+
+(baseline_runs |> 
+    select(Date, uQInEpi, NAME, WBID) |> 
+    mutate(doy = yday(Date),
+           year = year(Date)) |> 
+    
+    ggplot(aes(x=doy, y = uQInEpi)) + # obs are in ug/L
+    geom_line(aes(group = year), alpha = 0.6, colour = 'blue4') + # PClake in g/m3
+    theme_bw() +
+    facet_wrap(~WBID+NAME, scales = 'free_y')) 
 
 
 # calculate the TN:TP ratio
